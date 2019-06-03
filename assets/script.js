6775934c902d3f27cb6cfa9c10dd09930f41520f
Jonas Beinder <jonas@schmuhf.com>
2019-06-03 12:48:15 +0200

    navigation working. starting with map.

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
-var viewportWidth = $(window).width();
+/*var viewportWidth = $(window).width();
 var viewportHeight = $(window).height();
 
 let speed_x = (viewportWidth / 70);
-let speed_y = (viewportHeight / 105);
+let speed_y = (viewportHeight / 105);*/
 
 var player;
 function onYouTubeIframeAPIReady() {
@@ -26,8 +26,7 @@ function onYouTubeIframeAPIReady() {
 }
 
 function onPlayerReady(event) {
-    //event.target.playVideo();
-    player.play();
+    event.target.playVideo();
     frame();
 }
 
@@ -48,27 +47,49 @@ function frame() {
             duration = player.getDuration();
         }
 
-        if(player.getCurrentTime() > 5 && player.getCurrentTime() < 40) {
+        /*if(player.getCurrentTime() > 5 && player.getCurrentTime() < 40) {
             moveCablecar(player.getCurrentTime() - 5);
         }
         if(player.getCurrentTime() > 40 && player.getCurrentTime() < duration - 40) {
             moveLayer1(player.getCurrentTime() - 40);
-        }
+        }*/
+
         //console.log(player.getCurrentTime() + " of " + duration);
     }
     window.requestAnimationFrame(frame);
 }
 
-function moveCablecar(time) {
-    $("#cablecar")
-        .css("background-position",
-            "bottom " + (-475 + speed_y * time) + "px " +
-            "left " + (-2000 + speed_x * time) + "px");
+function setPlayerPosition(mark)
+{
+    player.seekTo(mark);
 }
 
-function moveLayer1(time) {
+var map;
+$("document").ready(function() {
+    var map = L.map('map').setView([51.505, -0.09], 13);
+    //https://api.mapbox.com/styles/v1/jonas-tum/cjwg8opkd01rl1cqqd685odt4/wmts?access_token=pk.eyJ1Ijoiam9uYXMtdHVtIiwiYSI6ImNqbm4yNGt2dzIyNHUzd29zaXdnZ2tnMzgifQ.dp77LW08dWvM3FD2olnfDg
+    L.tileLayer('https://api.mapbox.com/styles/v1/{user}/{id}/wmts?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        user: 'jonas-tum',
+        id: 'cjwg8opkd01rl1cqqd685odt4',
+        accessToken: 'pk.eyJ1Ijoiam9uYXMtdHVtIiwiYSI6ImNqbm4yNGt2dzIyNHUzd29zaXdnZ2tnMzgifQ.dp77LW08dWvM3FD2olnfDg'
+    }).addTo(map);
+});
+
+
+/*function moveCablecar(time)
+{
+   ("#cablecar")
+       .css("background-position",
+           "bottom " + (-475 + speed_y * time) + "px " +
+           "left " + (-2000 + speed_x * time) + "px");
+}
+
+function moveLayer1(time)
+{
     $("#layer1")
         .css("background-position",
             "bottom " + (-speed_y * time) + "px " +
             "left " + (-20 - speed_x * time) + "px");
-}
+}*/
